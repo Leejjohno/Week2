965a4590255ff88b612ec8b1ece0cfffea9dae55
Leejjohno <leej67885@gmail.com>
2022-04-19 12:43:46 +0100

    tuesday1243

---
+++ Week2/tuesdayCodeAlong.js
@@ -0,0 +1,25 @@
+// function reverseString(str) {
+//     return str.split("").reverse().join("")
+// }
+
+// console.log(reverseString("this should work!"))
+
+//  ----------------------activity (concat shopping list)
+
+// const arr1 = ["bread", "wraps", "pasta"]
+// const arr2 = ["onions", "mushrooms", "lettuce"]
+// const arr3 = ["chocolate", "sweets", "more sweets"]
+
+// const shoppingList = arr1.concat(arr2,arr3)
+// console.log(shoppingList[5])
+
+//  ------------------------(solution 2 without concat)
+
+// let shoppingList = [
+//     ['cola', 'fanta', 'tequilla'],
+//     ['crisps', 'onion rings', 'sticks'],
+//     ['roses','dandofils','flowers'],
+// ];
+
+// console.log( 'no concat: ', shoppingList[1][2]);
+// console.log( 'sub-array: ', shoppingList[1] )
